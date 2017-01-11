ac9e1b4ae207f54d9db8f42c00b77a096a0bbf6b
olaoluwa <olorundaolaoluwa@gmail.com>
2017-01-11 14:25:06 +0100

    ui changes

--- resources/views/partials/empprofile.blade.php
+++ resources/views/partials/empprofile.blade.php
@@ -1,3 +1,7 @@
+<style>
+
+
+</style>
 <div class="modal fade modal-slide-in-right" id="viewemp{{$employee->id}}" aria-labelledby="exampleModalTitle" role="dialog" tabindex="-1" aria-hidden="true" style="display: none;">
                     <div class="modal-dialog modal-lg">
                       <div class="modal-content">
@@ -16,35 +20,34 @@
 								<div class="col-md-9">
 									<h4>{{strtoupper($employee->name)}}</h4>
 									<?php $job = app('App\Http\Controllers\EmployeeController')->getjobdetail($employee->job_id); ?>
-									<h5>Job Role: {{strtoupper($job['title'])}}</h5>
-									<h5> ID: {{strtoupper($employee->emp_num)}}</h5>
-									<h5>Department: {{app('App\Http\Controllers\AvailJobController')->getDept($employee->workdept_id)}}</h5>
-									<h5>Last Promoted on: {{niceDate($employee->last_promoted)}}</h5>
+									<h5><b>Job Role:</b> {{($job['title'])}}</h5>
+									<h5><b style="font-weight:bold;"> ID: {{strtoupper($employee->emp_num)}}</b></h5>
+									<h5><b>Department: </b>{{app('App\Http\Controllers\AvailJobController')->getDept($employee->workdept_id)}}</h5>
+									<h5><b>Last Promoted on:</b> {{niceDate($employee->last_promoted)}}</h5>
 								</div>
 							</div>
 							<hr>
 							<div class="row">
 								<div class="col-md-6">
 									<dl class="dl-horizontal">
-										<dt>E-Mail</dt>
+										<dt><b style="font-weight:bold;">E-Mail</b></dt>
 										<dd>{{$employee->email}}</dd>
-
-										<dt>Phone</dt>
+										<dt style="font-weight:bold;">Phone</dt>
 										<dd>{{$employee->phone_num}}</dd>
 									</dl>
 								</div>
 								<div class="col-md-6">
 									<dl class="dl-horizontal">
-										<dt>Sex</dt>
+										<dt ><b style="font-weight:bold;">Sex</b></dt>
 										<dd>{{$employee->sex}}</dd>
 
-										<dt>Date of Birth</dt>
+										<dt><b style="font-weight:bold;">Date of Birth</b></dt>
 										<dd>{{$employee->dob}} ({{$employee->age}})</dd>
 									</dl>
 								</div>
 								<div class="col-md-12">
 									<dl class="dl-horizontal">
-										<dt>Address</dt>
+										<dt ><b style="font-weight:bold;">Address</b></dt>
 										<dd>{{$employee->address}}</dd>
 									</dl>
 								</div>
@@ -53,22 +56,22 @@
 							<div class="row">
 								<div class="col-md-6">
 									<dl class="dl-horizontal">
-										<dt>Next of Kin</dt>
+										<dt ><b style="font-weight:bold;">Next of Kin</b></dt>
 										<dd>{{$employee->next_of_kin}}</dd>
 
-										<dt>Relationship</dt>
+										<dt><b style="font-weight:bold;">Relationship</b></dt>
 										<dd>{{$employee->kin_relationship}}</dd>
 									</dl>
 								</div>
 								<div class="col-md-6">
 									<dl class="dl-horizontal">
-										<dt>Phone</dt>
+										<dt style="font-weight:bold;"><b style="font-weight:bold;">Phone</b></dt>
 										<dd>{{$employee->kin_phonenum}}</dd>
 									</dl>
 								</div>
 								<div class="col-md-12">
 									<dl class="dl-horizontal">
-										<dt>Address</dt>
+										<dt style="font-weight:bold;"><b style="font-weight:bold;">Address</b></dt>
 										<dd>{{$employee->kin_address}}</dd>
 									</dl>
 								</div>
